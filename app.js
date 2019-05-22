191bc7971585bd6b4305f696c0bb0977b86d1db9
Bobby <h122656227@gmail.com>
2019-05-22 23:33:26 +0800

    update server, init routes

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
 
+//将路由文件引入
+const route = require('./routes/index');
+
+//初始化所有路由
+route(app);
+
 module.exports = app
---
+++ routes/user.js
@@ -0,0 +1,4 @@
+exports.login =(req,res) => {
+   // let {username,password} = req.body
+    res.send('Hi 你好~')
+}
