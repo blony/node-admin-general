48c5598f21c8516986a5d2f1cb45e84ae450d422
Bobby <h122656227@gmail.com>
2019-05-23 17:03:17 +0800

    update port, login API

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,9 @@
 exports.login =(req,res) => {
    // let {username,password} = req.body
-    res.send('Hi 你好~')
+  res.json({
+      code:20000,
+      data:{
+          token:'admin-token'
+      }
+  })
 }
